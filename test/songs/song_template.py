2b0dec30c5582bffeb98f01ea8f9905179e4220a
Rémy Lapointe <28580319+OmegaLarmor@users.noreply.github.com>
2022-10-17 14:02:33 -0400

    Add song creation template file

---
+++ test/songs/song_template.py
@@ -0,0 +1,16 @@
+from song import *
+from beats import *
+
+########### Metadata ###########
+metadata = Metadata(
+        workTitle = $workTitle$
+    )
+########### End Metadata ###########
+
+
+########### Song creation ###########
+
+# Fill up this array with Measure objects
+measures = []
+
+########### End song creation ###########
