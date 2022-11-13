2ada31fdca67d88b06a46ddf044abc4d8180ece3
Rémy Lapointe <28580319+OmegaLarmor@users.noreply.github.com>
2022-11-13 15:57:27 -0500

    Again

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ html_static_path = []
 
 
 # -- Add all relevant modules to PATH for autodoct ---------------------------
-root_path = os.path.join(os.path.abspath('.'), "pydrumscore")
-root_path = os.path.join(os.path.abspath('.'), "pydrumscore", "core")
-sys.path.append(root_path)
+# root_path = os.path.join(os.path.abspath('.'), "pydrumscore")
+# root_path = os.path.join(os.path.abspath('.'), "pydrumscore", "core")
+# sys.path.append(root_path)
 sys.path.insert(0, os.path.abspath('.'))
