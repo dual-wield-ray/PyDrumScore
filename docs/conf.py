ec271faa10e2be571bd01f4c0b0d809335d824a6
Rémy Lapointe <28580319+OmegaLarmor@users.noreply.github.com>
2022-11-13 15:46:36 -0500

    Try adding build rst step

--- docs/conf.py
+++ docs/conf.py
@@ -33,14 +33,7 @@ html_static_path = []
 
 
 # -- Add all relevant modules to PATH for autodoct ---------------------------
-# TODO: Should be automatic somehow
-# sys.path.insert(0, os.path.abspath('.'))
+root_path = os.path.join(os.path.abspath('..'), "pydrumscore")
 root_path = os.path.join(os.path.abspath('..'), "pydrumscore", "core")
 sys.path.append(root_path)
 sys.path.insert(0, os.path.abspath('.'))
-# sys.path.insert(0, os.path.join(os.path.abspath('.'), "pydrumscore"))
-# sys.path.insert(0, os.path.join(os.path.abspath('..'), "pydrumscore", "core"))
-
-# import os
-# input_path = os.path.abspath('..')
-# print(os.listdir(input_path))
