941654a6f1c1b3fe3fc6b9f51c6b789394a60bfd
Rémy Lapointe <28580319+OmegaLarmor@users.noreply.github.com>
2022-11-13 14:24:40 -0500

    Move conf.py for pipe

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ html_static_path = ['_static']
 
 # -- Add all relevant modules to PATH for autodoct ---------------------------
 # TODO: Should be automatic somehow
-root_path = os.path.join(os.path.dirname(__file__), "pydrumscore")
-root_path = os.path.join(os.path.dirname(__file__), "pydrumscore", "core")
+root_path = os.path.join(os.path.abspath('..'), "pydrumscore")
+root_path = os.path.join(os.path.abspath('..'), "pydrumscore", "core")
 sys.path.append(root_path)
 sys.path.insert(0, os.path.abspath('.'))
