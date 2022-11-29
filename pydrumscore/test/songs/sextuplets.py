2eac0a971edd7ef9ce299dac50e97f3176cdfe39
Rémy Lapointe <28580319+OmegaLarmor@users.noreply.github.com>
2022-11-29 14:20:39 -0500

    Put back file

---
+++ pydrumscore/test/songs/sextuplets.py
@@ -0,0 +1,13 @@
+# pylint: disable = missing-module-docstring
+
+from pydrumscore import Measure, note_range, Metadata
+
+metadata = Metadata(
+    workTitle = "Sextuplets",
+    )
+
+measures = []
+
+measures += Measure(
+    sd = note_range(1, 2, 1.0/6.0)
+)
