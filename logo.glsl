2aaf7cb08022761b9f07f0264d526c2940cb02ff
Rémy Lapointe <28580319+OmegaLarmor@users.noreply.github.com>
2022-12-05 17:21:05 -0500

    Further dev, initial export of draft

--- logo.glsl
+++ logo.glsl
@@ -4,16 +4,13 @@ precision mediump float;
 #endif
 
 uniform vec2 u_resolution;
+uniform float u_time;
 
 float stroke(float x, float s, float w){
     float d = step(s, x+w*.5)
             - step(s, x-w*.5);
 
     return clamp(d, 0., 1.);
-}
-
-vec3 rgb(float r, float g, float b) {
-	return vec3(r / 255.0, g / 255.0, b / 255.0);
 }
 
 float circle(vec2 uv, vec2 pos, float rad) {
@@ -27,39 +24,40 @@ float circle(vec2 uv, vec2 pos, float rad) {
                          d);
 }
 
-vec4 snake(vec2 uv, vec2 pos, float rad, vec3 color, vec3 inside_col) {
+vec4 snake(vec2 uv, vec2 pos, float rad, vec3 color) {
 
+    // Get pixel info
     vec2 pos_diff = pos - uv;
     float theta = atan(pos_diff.y, pos_diff.x);
     float d = length(pos_diff);
 
     // Tail smoothing
-    float tail_start = radians(0.);
-    float tail_end = radians(15.);
+    float tail_start = radians(0. - u_time * 5.);
+    float tail_end = tail_start + radians(45.);
     float tail_var = smoothstep(tail_start, tail_end, theta);
     tail_var *= 1. - step(tail_end, theta);  // no tail var past the range
     
     // Head
     float head_start = tail_end;
-    float head_middle = radians(25.0);
-    float head_end = radians(35.0);
-    float head_var = smoothstep(head_start, head_middle, theta);
-    head_var -= smoothstep(head_middle, head_end, theta);
+    float head_middle = head_start + radians(10.0);
+    float head_end = head_middle + radians(5.0);
+    float head_var = smoothstep(head_start, head_middle, theta)
+                   - smoothstep(head_middle, head_end, theta);
     head_var *= 1. - step(head_end, theta);
 
     // Main wave
-    float amp = 2.0;
+    float amp = 4.0;
     float period = 12.0;
     
     // Offset to get the right look
-    float wave_offset = radians(270.);
+    float wave_offset = radians(270. + u_time * 150.);
     
     // Offset radian val by sine wave
     rad += sin(period * theta + wave_offset) * amp;
     
     // Get the value with the head and tail vars
-    float tail_mult = 5.;
-    float head_mult = 7.0;
+    float tail_mult = 8.;
+    float head_mult = 15.;
     float rad_t = rad - tail_var * tail_mult
                       + head_var * head_mult;
           
@@ -69,42 +67,54 @@ vec4 snake(vec2 uv, vec2 pos, float rad, vec3 color, vec3 inside_col) {
                          d);
 
     // Inner section
-    float thickness = 25.5;
+    float thickness = 35.5;
+
+    // thickness *= tail_var;
+
     rad -= thickness;
-    rad_t = rad + tail_var * tail_mult - head_var * head_mult;
+    rad_t = rad + (tail_var * tail_mult) - (head_var * head_mult);
 	float t2 = 1. -smoothstep(rad_t-(rad_t*0.01),
                          rad_t+(rad_t*0.01),
                          d);
 
     // Subtract to get only within the snake
-    //t = t - t2;
-          
-    vec3 res_color = mix(color, inside_col, t * t2);
+    t = t - t2;
 
-    return vec4(res_color, t);
+    return vec4(color, t);
 }
 
 void main() {
 
 	vec2 uv = gl_FragCoord.xy;
+    vec2 st = gl_FragCoord.xy/u_resolution;
 	vec2 center = u_resolution.xy * 0.5;
-    
-	float radius = 0.35 * u_resolution.y;
 
     // Background layer
-	vec4 layer1 = vec4(rgb(255., 255., 255.), 1.0);
+	vec4 layer1 = vec4(vec3(1.), 1.0);
 	
-	vec3 snake_color = vec3(0.643,0.643,0.643);
-    vec3 inside_color = vec3(1.,1.,1.);
-	vec4 layer2 = snake(uv, center, radius, snake_color, inside_color);
-	
-    // Middle circle
-    float middle_rad = u_resolution.y * 0.285;
-    float in_middle = circle(uv, center, middle_rad);
-    vec3 middle_color = vec3(0.89,0.855,0.788);
-    vec4 layer3 = vec4(middle_color, 1.);
+    // Snake diagram
+	vec3 snake_color = vec3(0.4078, 0.5373, 0.3333);
+	float radius = 0.35 * u_resolution.y;
+	vec4 layer2 = snake(uv, center, radius, snake_color);
 
-	// Blend the two
+    // Drumsticks
+    float drumstick_thickness = 0.035;
+    float sdf = 0.5 + (st.x - 1.75*st.y)*.5;
+    float is_drumstick = stroke(sdf, 0.5, drumstick_thickness);
+    float sdf_inv = (st.x + 1.75*st.y)*.5;
+    is_drumstick += stroke(sdf_inv, .5, drumstick_thickness);
+    is_drumstick = clamp(is_drumstick, 0., 1.);
+
+    is_drumstick *= step(0.1, st.x);
+    is_drumstick *= 1. - step(0.9, st.x);
+    is_drumstick *= step(0.15, st.y);
+
+    vec4 layer3 = vec4(vec3(0.8431, 0.7412, 0.4745), is_drumstick);
+
+
+	// Blend background and snake
 	gl_FragColor = mix(layer1, layer2, layer2.a);
+
+    gl_FragColor = mix(gl_FragColor, layer3, layer3.a);
 
 }
