ef392b604ce009ce18d725714eb4e2b58d7451d3
dual-wield-ray <dual-wield-ray@users.noreply.github.com>
2022-12-13 15:13:13 +0000

    Automated deployment: Tue Dec 13 15:13:13 UTC 2022 2da0786240909e500ef25d0ce68b87a45cb71121

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.0.5.dev30+gadbc246',
+    VERSION: '0.0.5.dev35+g2da0786',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'dirhtml',
