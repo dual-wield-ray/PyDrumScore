4c05a9a306645259eca78f6dd48925849de68788
DualWield <28580319+OmegaLarmor@users.noreply.github.com>
2022-12-23 19:04:11 -0500

    First file that is openable, beam management will be an issue

--- pydrumscore/__main__.py
+++ pydrumscore/__main__.py
@@ -2,6 +2,7 @@
 Calls the exporter when running 'pydrumscore' from the command line with 'python -m pydrumscore'
 """
 from pydrumscore import export
+from pydrumscore import export_musicxml
 
 if __name__ == "__main__":
-    export.main()
+    export_musicxml.main()
